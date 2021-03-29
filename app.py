1391c7fe7994e037e97650649b8d9a9c5029ce6e
Xinyi Wu <xinyiwu210@gmail.com>
2021-03-29 15:48:19 -0500

    vtm_site initial commit

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+
 
 app = Flask(__name__)
+
+def calc(radius, height):
+    areaTop = (3.14 * radius**2)
+    areaSides = radius*height*3.14*2
+    areaTotal = areaTop + areaSides
+    sqFeet = areaTotal/144
+    mCost = 25 * sqFeet
+    lCost = 15 *sqFeet
+    totalCost = round(mCost + lCost,2)
+    print(totalCost)
+    return totalCost
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['Get'])
 def estimate():
     return render_template('estimate.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        radius = int(request.form['radius'])
+        height = int(request.form['height'])
+        print(radius)
+        print(height)
+        estimateCost = calc(radius, height)
+        #estimateCost = totalCost
+        print(estimateCost)
+    return render_template('estimate.html', estimate=estimateCost) 
 
 if __name__ == '__main__':
     app.run(debug=True)
